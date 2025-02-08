2a18dcaa444d7c27b9d95d9439bb250953b1df69
santiagomanganiello <santiagomanganiello@gmail.com>
2025-02-08 08:46:25 -0300

    sesion 3 ultimo ejercicio, primeros de la sesión 4

---
+++ Sesión 4/Familiarización con secuencias/secuencias_2.py
@@ -0,0 +1,12 @@
+# Crear una secuencia con números distintos, y luego devolver el 
+# elemento máximo y el minimo.
+
+lista = [40, 25, 80, 3, 5, 9, 11, 100, 15, 30]
+
+print(f"El numero maximo de la lista es: {max(lista)}")
+print(f"El numero minimo de la lista es: {min(lista)}")
+
+# Ordenar la secuencia del ejercicio anterior, e imprimirla por pantalla.
+
+lista.sort()
+print(f"La lista ordenada es: {lista}")
