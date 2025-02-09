c4f8054df6f048b9a7cf279b7badf83c4a46b481
santiagomanganiello <santiagomanganiello@gmail.com>
2025-02-09 09:16:07 -0300

    listas y tuplas, sesion 4

---
+++ Sesión 4/Listas y Tuplas/tuplas_3.py
@@ -0,0 +1,11 @@
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+listaAlCuadrado = []
+
+for num in lista:
+    listaAlCuadrado.append(num * num)
+
+
+print(f"La lista es: {lista}")
+print(f"La lista al cuadrado es: {listaAlCuadrado}")
